8ec42e8a54520108db442e995d2722fc0ed95b4d
Etiene Domingos <etienedaniel32@gmail.com>
2025-05-29 22:54:57 +0100

    testando a conexao do banco em producao

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
+
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Conexão com banco OK!';
+    } catch (\Exception $e) {
+        return 'Erro na conexão: ' . $e->getMessage();
+    }
 });
